5020e3144267af440cd10f75f84bffc05c03a0ff
Tay Weida <weida.tay99@gmail.com>
2021-09-20 14:58:17 +0800

    add more code to print using stream

--- src/se/edu/inclass/Main.java
+++ src/se/edu/inclass/Main.java
@@ -20,6 +20,8 @@ public class Main {
         printDeadlines(tasksData);
 
         System.out.println("Total number of deadlines: " + countDeadlines(tasksData));
+        System.out.println("Total number of deadlines using stream: "
+                + countDeadlinesUsingStream(tasksData));
 
     }
 
@@ -30,6 +32,13 @@ public class Main {
                 count++;
             }
         }
+        return count;
+    }
+
+    private static int countDeadlinesUsingStream(ArrayList<Task> tasks) {
+        int count = (int) tasks.stream()
+                .filter(t -> t instanceof Deadline)
+                .count();
         return count;
     }
 
